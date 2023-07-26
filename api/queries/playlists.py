00a3aa56ee61a262b793ba6b4c434c444ba9a73a
Tom <yaokiyamaguchi@icloud.com>
2023-07-25 18:02:34 -0700

    fixed the add song to playlist endponts, and front end functionality

--- api/queries/playlists.py
+++ api/queries/playlists.py
@@ -2,7 +2,7 @@ import os
 from typing import Optional, Union, List
 from .pool import pool
 from pydantic import BaseModel, validator
-from fastapi import UploadFile, HTTPException
+from fastapi import UploadFile, HTTPException, File
 from botocore.exceptions import NoCredentialsError
 import uuid
 import boto3
@@ -10,7 +10,6 @@ import boto3
 
 class Error(BaseModel):
     message: str
-
 
 class PlaylistIn(BaseModel):
     name: str
@@ -33,8 +32,14 @@ class PlaylistOut(BaseModel):
     name: str
     owner: str
     description: Optional[str] = None
-    songs: List[str] = []
+    songs: Optional[List[str]] = []
+
+    artists: Optional[List[str]]
+    music_files: Optional[List[str]]
+    covers: Optional[List[str]]
+    durations: Optional[List[str]]
     cover_url: str
+
 
 
 class PlaylistRepository:
@@ -60,7 +65,7 @@ class PlaylistRepository:
             f.write(await file_name.read())
 
         try:
-            self.s3_client.upload_file(temp, bucket, object_name)
+            response = self.s3_client.upload_file(temp, bucket, object_name)
             print("Upload Successful")
             return True
         except NoCredentialsError:
@@ -97,7 +102,11 @@ class PlaylistRepository:
                             p.name,
                             u.username as owner,
                             p.description,
-                            STRING_AGG(DISTINCT CAST(ps.song_id AS TEXT), ',') as songs,
+                            array_agg(s.title) FILTER (WHERE s.title IS NOT NULL) as songs,
+                            array_agg(s.artist) FILTER (WHERE s.artist IS NOT NULL) as artists,
+                            array_agg(s.music_file) FILTER (WHERE s.music_file IS NOT NULL) as music_files,
+                            array_agg(s.cover) FILTER (WHERE s.cover IS NOT NULL) as covers,
+                            array_agg(s.duration) FILTER (WHERE s.duration IS NOT NULL) as durations,
                             p.cover
                         FROM playlists p
                         LEFT JOIN playlist_songs ps ON p.id = ps.playlist_id
@@ -115,8 +124,12 @@ class PlaylistRepository:
                             name=record[1],
                             owner=record[2],
                             description=record[3],
-                            songs=record[4].split(",") if record[4] else [],
-                            cover_url=record[5],
+                            songs=record[4],
+                            artists=record[5],
+                            music_files=record[6],
+                            covers=record[7],
+                            durations=record[8],
+                            cover_url=record[9],
                         )
                     return None
         except Exception as e:
@@ -134,7 +147,7 @@ class PlaylistRepository:
                             p.name,
                             u.username as owner,
                             p.description,
-                            STRING_AGG(DISTINCT p.name, ',') as songs,
+                            STRING_AGG(DISTINCT s.title, ',') as songs,
                             p.cover
                         FROM playlists p
                         LEFT JOIN playlist_songs ps ON p.id = ps.playlist_id
@@ -336,4 +349,80 @@ class PlaylistRepository:
                     return True
         except Exception as e:
             print(f"Error creating playlist song: {e}")
+            return False
+
+
+    async def search_for_songs(self, title: str):
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        SELECT id, title FROM songs WHERE title LIKE %s
+                        """,
+                        [f"%{title}%"],
+                    )
+                    songs = db.fetchall()
+            return songs
+        except Exception as e:
+            print(f"Error searching for songs: {e}")
+            raise HTTPException(
+                status_code=500, detail="Internal server error."
+            )
+
+
+
+
+    async def add_song_to_playlist(self, playlist_id: int, song_id: int):
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        INSERT INTO playlist_songs (playlist_id, song_id)
+                        VALUES (%s, %s);
+                        """,
+                        [playlist_id, song_id],
+                    )
+
+
+                    db.execute(
+                        """
+                        SELECT * FROM songs WHERE id = %s;
+                        """,
+                        [song_id],
+                    )
+                    row = db.fetchone()
+
+                    added_song = {
+                        "id": row[0],
+                        "title": row[2],
+                        "artist": row[3],
+                        "music_file": row[4],
+                        "cover": row[5],
+                        "duration": row[6],
+                    }
+
+            return added_song
+        except Exception as e:
+            print(f"Error adding song to playlist: {e}")
+            return False
+
+    async def check_song_in_playlist(self, playlist_id: int, song_id: int) -> bool:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        SELECT * FROM playlist_songs
+                        WHERE playlist_id = %s AND song_id = %s;
+                        """,
+                        [playlist_id, song_id],
+                    )
+                    song_in_playlist = db.fetchone()
+
+
+            return bool(song_in_playlist)
+        except Exception as e:
+            print(f"Error checking if song is in playlist: {e}")
             return False
--- ghi/src/components/playlists/PlaylistDetail.js
+++ ghi/src/components/playlists/PlaylistDetail.js
@@ -5,26 +5,30 @@ import useToken from "@galvanize-inc/jwtdown-for-react";
 export default function PlaylistDetail() {
   const { token } = useToken();
   const { playlistId } = useParams();
+  const { songId } = useParams();
   const [playlist, setPlaylist] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchPlaylist = async () => {
-      try {
-        const response = await fetch(
-          `${process.env.REACT_APP_API_HOST}/playlists/${playlistId}`
-        );
-        if (response.ok) {
-          const data = await response.json();
-          setPlaylist(data);
-        } else {
-          console.log("Failed to fetch playlist");
-        }
-      } catch (error) {
-        console.log("Error fetching playlist:", error.message);
+  const fetchPlaylist = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_HOST}/playlists/${playlistId}`
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setPlaylist(data);
+      } else {
+        console.log("Failed to fetch playlist");
       }
-    };
+    } catch (error) {
+      console.log("Error fetching playlist:", error.message);
+    }
+  };
 
+  // Use fetchPlaylist in the useEffect
+  useEffect(() => {
     fetchPlaylist();
   }, [playlistId]);
 
@@ -56,8 +60,135 @@ export default function PlaylistDetail() {
     return `${minutes}:${String(seconds).padStart(2, "0")}`;
   };
 
+  const searchSongs = async () => {
+    // Log searchTerm before the fetch call
+    console.log(`Search term: ${searchTerm}`);
+    const requestUrl = `${
+      process.env.REACT_APP_API_HOST
+    }/songs?title=${encodeURIComponent(searchTerm)}`;
+    console.log(`Request URL: ${requestUrl}`);
+
+    try {
+      const response = await fetch(requestUrl, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        const songs = await response.json();
+        setSearchResults(songs);
+
+        // Log search results after they've been fetched
+        console.log("Search results:", songs);
+      } else {
+        console.log("Song search failed.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const addSongToPlaylist = async (songId) => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_HOST}/playlists/${playlistId}/add_song/${songId}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({ songId }),
+        }
+      );
+
+      if (response.ok) {
+        console.log(`Song with id: ${songId} was added to the playlist.`);
+        // Reload playlist songs after adding a new one
+        fetchPlaylist();
+      } else {
+        console.log("Song could not be added to the playlist.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="bg-black text-white min-h-screen p-10 font-sans">
+      <div className="flex justify-between items-start mb-5">
+        {playlist && (
+          <>
+            <div className="flex items-center">
+              <div className="flex justify-center items-center bg-gray-900 rounded w-72 h-72">
+                <Link to={`/playlists/${playlist.id}`}>
+                  <img
+                    className="w-64 h-64 mb-2 object-cover rounded"
+                    src={playlist.cover_url}
+                    alt={playlist.name}
+                  />
+                </Link>
+              </div>
+              <div className="ml-5">
+                <h1 className="text-4xl mb-5 font-semibold">{playlist.name}</h1>
+                <p className="text-base font-bold">{playlist.description}</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-start">
+              <div>
+                <input
+                  type="text"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button onClick={searchSongs}>Search</button>
+              </div>
+              {searchResults.length > 0 && (
+                <div>
+                  {searchResults.map((song) => (
+                    <div
+                      key={song.id}
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        padding: "10px",
+                        borderRadius: "5px",
+                        transition: "0.3s",
+                      }}
+                      onMouseEnter={(e) =>
+                        (e.currentTarget.style.backgroundColor = "#ddd")
+                      }
+                      onMouseLeave={(e) =>
+                        (e.currentTarget.style.backgroundColor = "transparent")
+                      }
+                    >
+                      <p>{song.title}</p>
+                      <button
+                        onClick={() => addSongToPlaylist(song.id)}
+                        style={{
+                          fontSize: "0.7rem",
+                          padding: "0.5rem",
+                          backgroundColor: "#007BFF",
+                          color: "#fff",
+                          border: "none",
+                          borderRadius: "5px",
+                          cursor: "pointer",
+                        }}
+                      >
+                        Add Song to Playlist
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </>
+        )}
+      </div>
+
       <div className="border border-gray-800 p-4 rounded-lg mb-6 bg-gray-800">
         <div className="controlBar flex justify-between">
           <Link
@@ -74,92 +205,72 @@ export default function PlaylistDetail() {
           </button>
         </div>
 
-        <div className="flex justify-start items-start mb-5">
-          {playlist && (
-            <>
-              <div className="flex justify-center items-center bg-gray-900 rounded w-72 h-72">
-                <Link to={`/playlists/${playlist.id}`}>
-                  <img
-                    className="w-64 h-64 mb-2 object-cover rounded"
-                    src={playlist.cover_url}
-                    alt={playlist.name}
-                  />
-                </Link>
-              </div>
-              <div className="ml-5">
-                <h1 className="text-4xl mb-5 font-semibold">{playlist.name}</h1>
-                <p className="text-base font-bold">{playlist.description}</p>
-              </div>
-            </>
+        <div className="border border-gray-800 p-4 rounded-lg bg-gray-800">
+          {playlist && playlist.songs && playlist.songs.length > 0 ? (
+            <table className="table-auto w-full">
+              <thead>
+                <tr>
+                  <th className="border px-2 py-2 text-lg font-semibold text-left">
+                    #
+                  </th>
+                  <th className="border px-2 py-2 text-lg font-semibold text-left">
+                    Cover
+                  </th>
+                  <th className="border px-2 py-2 text-lg font-semibold text-left">
+                    Title
+                  </th>
+                  <th className="border px-2 py-2 text-lg font-semibold text-left">
+                    Artist
+                  </th>
+                  <th className="border px-2 py-2 text-lg font-semibold text-left">
+                    Music File
+                  </th>
+                  <th className="border px-2 py-2 text-lg font-semibold text-left">
+                    <div style={{ fontFamily: "Montserrat, sans-serif" }}>
+                      Duration
+                    </div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {playlist.songs.map((song, index) => (
+                  <tr key={index}>
+                    <td className="border px-2 py-2 text-lg">{index + 1}</td>
+                    <td className="border px-2 py-2">
+                      <img
+                        className="w-10 h-10 object-cover rounded"
+                        src={playlist.covers[index]}
+                        alt={`Cover for ${song}`}
+                      />
+                    </td>
+                    <td className="border px-2 py-2">
+                      <ol style={{ margin: 0, padding: 0 }}>
+                        <li style={{ marginBottom: "10px" }}>{song}</li>
+                      </ol>
+                    </td>
+                    <td className="border px-2 py-2 text-lg">
+                      {playlist.artists[index]}
+                    </td>
+                    <td className="border px-2 py-2">
+                      <audio controls>
+                        <source
+                          src={playlist.music_files[index]}
+                          type="audio/mpeg"
+                        />
+                        Your browser does not support the audio element.
+                      </audio>
+                    </td>
+                    <td className="border px-2 py-2 text-lg">
+                      {formatDuration(playlist.durations[index])}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <div className="text-lg">No songs in the playlist yet.</div>
           )}
         </div>
-      </div>
-
-      <div className="border border-gray-800 p-4 rounded-lg bg-gray-800">
-        {playlist && playlist.songs && playlist.songs.length > 0 ? (
-          <table className="table-auto w-full">
-            <thead>
-              <tr>
-                <th className="border px-2 py-2 text-lg font-semibold text-left">
-                  #
-                </th>
-                <th className="border px-2 py-2 text-lg font-semibold text-left">
-                  Cover
-                </th>
-                <th className="border px-2 py-2 text-lg font-semibold text-left">
-                  Title
-                </th>
-                <th className="border px-2 py-2 text-lg font-semibold text-left">
-                  Artist
-                </th>
-                <th className="border px-2 py-2 text-lg font-semibold text-left">
-                  Music File
-                </th>
-                <th className="border px-2 py-2 text-lg font-semibold text-left">
-                  <div style={{ fontFamily: "Montserrat, sans-serif" }}>
-                    Duration
-                  </div>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {playlist.songs.map((song, index) => (
-                <tr key={index}>
-                  <td className="border px-2 py-2 text-lg">{index + 1}</td>
-                  <td className="border px-2 py-2">
-                    <img
-                      className="w-10 h-10 object-cover rounded"
-                      src={playlist.covers[index]}
-                      alt={`Cover for ${song}`}
-                    />
-                  </td>
-                  <td className="border px-2 py-2">
-                    <ol style={{ margin: 0, padding: 0 }}>
-                      <li style={{ marginBottom: "10px" }}>{song}</li>
-                    </ol>
-                  </td>
-                  <td className="border px-2 py-2 text-lg">
-                    {playlist.artists[index]}
-                  </td>
-                  <td className="border px-2 py-2">
-                    <audio controls>
-                      <source
-                        src={playlist.music_files[index]}
-                        type="audio/mpeg"
-                      />
-                      Your browser does not support the audio element.
-                    </audio>
-                  </td>
-                  <td className="border px-2 py-2 text-lg">
-                    {formatDuration(playlist.durations[index])}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <div className="text-lg">No songs in the playlist yet.</div>
-        )}
       </div>
     </div>
   );
