dcae7f2c456c4097c566ebba1a755792754d2f7d
Alex <aelliottmao@gmail.com>
2023-07-27 13:29:50 -0700

    lint issue

--- ghi/src/components/profile/ProfilePage.js
+++ ghi/src/components/profile/ProfilePage.js
@@ -434,7 +434,7 @@ const ProfilePage = () => {
                     </button>
                   </li>
                   <li role="presentation">
-                    <a
+                    <button
                       onClick={() => handleTabClick("Friends")}
                       className="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
                       role="tab"
@@ -442,7 +442,7 @@ const ProfilePage = () => {
                       aria-selected="false"
                     >
                       Friends
-                    </a>
+                    </button>
                   </li>
                   <div className="flex items-center ml-auto">
                     {userNotFound && (
