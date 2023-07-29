23674486cc62170267166ef6a6e9c42859fddbdf
Tom <yaokiyamaguchi@icloud.com>
2023-07-28 21:11:04 -0700

    linear-gradient(to bottom right, #3023AE, #C86DD7),

--- ghi/src/components/playlists/Playlists.js
+++ ghi/src/components/playlists/Playlists.js
@@ -12,10 +12,19 @@ const Playlists = () => {
   }, []);
 
   return (
-    <div className="min-h-screen p-12 flex flex-col items-center">
-      <div className="bg-gradient-to-b from-slate-800 via-slate-500 via-gray-400 to-slate-300 w-full rounded-lg p-8">
+    <>
+      <div
+        className="relative pt-1 mx-auto w-screen text-center text-4x4 font-bold"
+        style={{
+          minHeight: "100vh",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          paddingBottom: "150px",
+          backgroundImage: `linear-gradient(109.6deg, rgb(48, 35, 174) 11.2%, rgb(200, 109, 215) 100.2%)`,
+        }}
+      >
         <div className="flex justify-between items-center w-full mb-10">
-
           <Link
             to="/create_playlist"
             className="flex justify-center bg-purple-800 hover:bg-purple-400 text-gray-100 p-3 rounded-md tracking-wide font-medium shadow-lg cursor-pointer transition ease-in duration-500 text-base"
@@ -24,11 +33,15 @@ const Playlists = () => {
           </Link>
         </div>
 
-        <div className="grid grid-cols-6 gap-10 mt-10 w-full">
+        <div className="grid grid-cols-5 gap-10 mt-10 w-full">
           {playlists.map((playlist) => (
             <div
               key={playlist.id}
               className="flex flex-col items-center w-30 h-30 rounded-lg relative transform transition duration-500 hover:scale-110 hover:rotate-6 hover:brightness-150 hover:shadow-2xl bg-slate-600 shadow-2xl p-5"
+              style={{
+                background:
+                  "linear-gradient(to bottom right, #3023AE, #C86DD7)",
+              }}
             >
               <div className="rounded-lg bg-purple-500 opacity-30 shadow-10x2 transition-colors duration-500 hover:bg-purple-300 hover:opacity-50"></div>
               <Link to={`/playlists/${playlist.id}`}>
@@ -47,8 +60,7 @@ const Playlists = () => {
           ))}
         </div>
       </div>
-
-    </div>
+    </>
   );
 };
 
